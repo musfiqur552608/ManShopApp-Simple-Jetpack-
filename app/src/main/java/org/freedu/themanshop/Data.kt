214f0adc4a9486ece712a002187d0688741a3476
Your NameMusfiqur <you@example.commushfiqur15-10872@diu.edu.bd>
2024-04-26 04:03:12 +0600

    Man shop app

---
+++ app/src/main/java/org/freedu/themanshop/Data.kt
@@ -0,0 +1,112 @@
+package org.freedu.themanshop
+
+import androidx.annotation.DrawableRes
+
+object Data {
+    class Demo(
+        @DrawableRes var img:Int,
+        var name:String,
+        var disc:String,
+        var price:String
+    )
+    
+    var dataList = listOf(
+        Demo(
+            R.drawable.man1,
+            name = "The Shirt",
+            disc = "The ever best shirt by us in market",
+            price = "$20"
+        ),
+        Demo(
+            R.drawable.man2,
+            name = "The Shirt",
+            disc = "The ever best shirt by us in market",
+            price = "$20"
+        ),
+        Demo(
+            R.drawable.man1,
+            name = "The Shirt",
+            disc = "The ever best shirt by us in market",
+            price = "$20"
+        ),
+        Demo(
+            R.drawable.man2,
+            name = "The Shirt",
+            disc = "The ever best shirt by us in market",
+            price = "$20"
+        ),
+        Demo(
+            R.drawable.man1,
+            name = "The Shirt",
+            disc = "The ever best shirt by us in market",
+            price = "$20"
+        ),
+        Demo(
+            R.drawable.man2,
+            name = "The Shirt",
+            disc = "The ever best shirt by us in market",
+            price = "$20"
+        ),
+        Demo(
+            R.drawable.man1,
+            name = "The Shirt",
+            disc = "The ever best shirt by us in market",
+            price = "$20"
+        ),
+        Demo(
+            R.drawable.man2,
+            name = "The Shirt",
+            disc = "The ever best shirt by us in market",
+            price = "$20"
+        ),
+        Demo(
+            R.drawable.man1,
+            name = "The Shirt",
+            disc = "The ever best shirt by us in market",
+            price = "$20"
+        ),
+        Demo(
+            R.drawable.man2,
+            name = "The Shirt",
+            disc = "The ever best shirt by us in market",
+            price = "$20"
+        ),
+        Demo(
+            R.drawable.man1,
+            name = "The Shirt",
+            disc = "The ever best shirt by us in market",
+            price = "$20"
+        ),
+        Demo(
+            R.drawable.man2,
+            name = "The Shirt",
+            disc = "The ever best shirt by us in market",
+            price = "$20"
+        ),
+        Demo(
+            R.drawable.man1,
+            name = "The Shirt",
+            disc = "The ever best shirt by us in market",
+            price = "$20"
+        ),
+        Demo(
+            R.drawable.man2,
+            name = "The Shirt",
+            disc = "The ever best shirt by us in market",
+            price = "$20"
+        ),
+        Demo(
+            R.drawable.man1,
+            name = "The Shirt",
+            disc = "The ever best shirt by us in market",
+            price = "$20"
+        ),
+        Demo(
+            R.drawable.man2,
+            name = "The Shirt",
+            disc = "The ever best shirt by us in market",
+            price = "$20"
+        )
+
+    )
+}
